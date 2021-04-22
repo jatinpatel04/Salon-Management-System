b05e402a02b0597a915acd7c67e540a4bd553e44
jatinpatel04 <82992746+jatinpatel04@users.noreply.github.com>
2021-04-22 10:47:47 +0530

    Add files via upload

---
+++ SMS/includes/header.php
@@ -0,0 +1,18 @@
+<nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="index.php">SALON MANAGEMENT SYSTEM</a>
+       
+        <div class="collapse navbar-collapse" id="ftco-nav">
+          <ul class="navbar-nav">
+            <li class="nav-item active"><a href="index.php" class="nav-link">Home</a></li>
+            <li class="nav-item"><a href="services.php" class="nav-link">Facilities</a></li>
+            
+            <li class="nav-item"><a href="about.php" class="nav-link">About</a></li>
+           
+            <li class="nav-item"><a href="contact.php" class="nav-link">Contact</a></li>
+            
+            <li class="nav-item"><a href="admin/index.php" class="nav-link">Admin</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
